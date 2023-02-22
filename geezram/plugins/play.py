dbfc7884407327eb8bc2bb2d9e2e3c137dab1e52
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-02-23 06:15:15 +0700

    Update play.py

--- geezram/plugins/play.py
+++ geezram/plugins/play.py
@@ -9,7 +9,7 @@ from pyrogram.types import (InlineKeyboardMarkup, InputMediaPhoto,
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from config import BANNED_USERS, lyrical, MUST_JOIN, OWNER_USERNAME
+from config import BANNED_USERS, lyrical, OWNER_USERNAME
 from strings import get_command
 from geezram import (Apple, Resso, SoundCloud, Spotify, Telegram,
                         YouTube, app)
@@ -32,43 +32,6 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from geezram import app
 
 
-def subcribe(func):
-    async def wrapper(_, message: Message):
-        user_id = message.from_user.id
-        user_name = message.from_user.first_name
-        rpk = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
-        if not MUST_JOIN:  # Not compulsory
-            return
-        try:
-            try:
-                await app.get_chat_member(MUST_JOIN, message.from_user.id)
-            except UserNotParticipant:
-                if MUST_JOIN.isalpha():
-                    link = "https://t.me/" + MUST_JOIN
-                else:
-                    chat_info = await app.get_chat(MUST_JOIN)
-                    chat_info.invite_link
-                try:
-                    await message.reply(
-                        f"ʜᴀʟʟᴏ {rpk}. ᴀɢᴀʀ ʙɪsᴀ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ʙᴏᴛ ᴀɴᴅᴀ ʜᴀʀᴜs sғs ᴋᴇ ᴄʜᴀɴɴᴇʟ ᴛᴇʀʟᴇʙɪʜ ᴅᴀʜᴜʟᴜ!. sɪʟᴀʜᴋᴀɴ ᴋʟɪᴋ ᴛᴏᴍʙᴏʟ ᴅɪ ʙᴀᴡᴀʜ ᴜɴᴛᴜᴋ ᴊᴏɪɴ ᴋᴇ ᴄʜᴀɴɴᴇʟ . sғs ʙᴀᴄᴋ sɪʟᴀᴋᴀɴ ᴄᴏɴᴛᴀᴄᴛ ᴏᴡɴᴇʀ ❤️**",
-                        disable_web_page_preview=True,
-                        reply_markup=InlineKeyboardMarkup(
-                            [[InlineKeyboardButton("❤️ Join Channel", url=link)]]
-                        ),
-                    )
-                    await message.stop_propagation()
-                except ChatWriteForbidden:
-                    pass
-        except ChatAdminRequired:
-            await message.reply(
-                f"Saya bukan admin di chat MUST_JOIN chat : {MUST_JOIN} !"
-            )
-        return await func(_, message)
-
-    return wrapper
-
-CHANNEL_ID = -1001818398503
-
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 @app.on_message(
@@ -77,7 +40,6 @@ PLAY_COMMAND = get_command("PLAY_COMMAND")
     & ~filters.edited
     & ~BANNED_USERS
 )
-@subcribe
 @PlayWrapper
 async def play_commnd(
     client,
